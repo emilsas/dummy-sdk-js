32884d5ce44db9109d52d022cba9d19e304aa489
Emil Santurio <emilsas@gmail.com>
2021-05-10 11:12:08 -0300

    Add dummy SDK

---
+++ docs-home.md
@@ -0,0 +1,2 @@
+# My Dummy SDK
+This is a test of Github actions to publish to NPM public registry and set a CI/CD flow.
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Alive from './Alive'
+import Apple from './Apple'
+import Energy from './Energy'
+import Environment from './Environment'
+import Tree from './Tree'
+
+export {
+  Alive,
+  Apple,
+  Energy,
+  Environment,
+  Tree
+}
