00c5f0efc9ca76d20da651ea3df52e8b4ae85f25
Emil Santurio <emilsas@gmail.com>
2021-05-10 14:36:14 -0300

    Add example in feature

--- src/Apple.js
+++ src/Apple.js
@@ -1,4 +1,5 @@
 /**
+ * Add change in feature!!!
  * Donec imperdiet dignissim semper. Sed vehicula purus dui, eget porta lectus convallis sagittis. Suspendisse ac lectus dignissim, tincidunt nisi quis, gravida metus.
  * @class
  * @extends Alive
