83159e33b71d98b95ef720b8f4084fe2b21190da
Emil Santurio <emilsas@gmail.com>
2021-05-11 11:06:53 -0300

    Add tests

---
+++ tests/Dummy.feature
@@ -0,0 +1,6 @@
+Feature: My dummy feature
+
+  Scenario: Test Jest works in Github actions
+    Given I have installed babel-jest
+    When I run a test
+    Then it works
---
+++ tests/step-definitions/Dummy.steps.js
@@ -0,0 +1,22 @@
+import { loadFeature, defineFeature } from 'jest-cucumber'
+const feature = loadFeature('../Dummy.feature', { loadRelativePath: true, errors: true })
+
+defineFeature(feature, test => {
+  test('Test Jest works in Github actions', ({ given, when, then }) => {
+    let a, b, result
+
+
+    given('I have installed babel-jest', () => {
+      a = 2
+      b = 2
+    })
+
+    when('I run a test', () => {
+      result = a + b
+    })
+
+    then('it works', () => {
+      expect(result).toBe(4)
+    })
+  })
+})
